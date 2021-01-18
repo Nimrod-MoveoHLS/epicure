03e584975e1f7f36317248728742aa304eff98bf
Nimrod-MoveoHLS <nimrod@moveohls.com>
2021-01-18 19:27:59 +0200

    Fixing navbar css

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./navBar.css";
 const navBar = () => {
   return (
-    <section className="navBar-container">
+    <section className="navbar-container">
+      <img
+        src="../images/group-13.png"
+        className="burger-icon"
+        alt="burger-icon"
+      ></img>
+
       <img src="../images/logo.png" className="logo-icon" alt="logo-icon"></img>
       <div className="logo">Epicure</div>
       <a href="/" className="menu-link">
@@ -17,11 +23,11 @@ const navBar = () => {
           placeholder="Search for restaurant cuisne, chef"
           className="search"
         ></input>
-              <img
-        src="../images/search-icon.png"
-        className="search-icon"
-        alt="search-icon"
-      ></img>
+        <img
+          src="../images/search-icon.png"
+          className="search-icon"
+          alt="search-icon"
+        ></img>
       </div>
       <img
         src="../images/user-icon.png"
